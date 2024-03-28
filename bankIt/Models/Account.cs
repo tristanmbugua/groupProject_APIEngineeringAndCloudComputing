70c1d9a68be4d32ba8b55c61523d91d4f2495aa2
nelsonle345 <nelsonle345@gmail.com>
2024-03-27 20:37:08 -0400

    I added the controller, interface, automapping, repository and dtos

---
+++ bankIt/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace bankIt.Models
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
---
+++ bankIt/Models/MonthlyCharge.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace bankIt.Models
+{
+    public class MonthlyCharge
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ChargeDate { get; set; }
+     
+    }
+}
