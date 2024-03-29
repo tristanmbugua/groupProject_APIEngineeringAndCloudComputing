0ddeaf730284736e7e5f776cb15459869b7415fd
nelsonle345 <97746929+nelsonle345@users.noreply.github.com>
2024-03-29 06:52:59 -0400

    Update CreditCardController.cs
    Edited the feedback for this Controller

--- bankIt/Controllers/CreditCardController.cs
+++ bankIt/Controllers/CreditCardController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace bankIt.Controllers
@@ -37,33 +37,39 @@ namespace bankIt.Controllers
         [HttpPut]
         public Object Put([FromBody] UpdateCredit request)
         {
-            SQLDriver.cmd.CommandText = $"SELECT creditCard FROM bankIt.accounts WHERE username = \"{request.username}\" && password = \"{request.password}\";";
-            var result = SQLDriver.cmd.ExecuteReaderAsync().Result;
-
-            result.Read();
-            int val = Convert.ToInt32(result[0]);
-            result.Close();
-
-
-            if (request.credit < 0)
-            {
-                SQLDriver.cmd.CommandText = $"SELECT creditcardFees FROM bankIt.accounts WHERE username = \"{request.username}\" && password = \"{request.password}\";";
-                result = SQLDriver.cmd.ExecuteReaderAsync().Result;
+            try {
+                SQLDriver.cmd.CommandText = $"SELECT creditCard FROM bankIt.accounts WHERE username = \"{request.username}\" && password = \"{request.password}\";";
+                var result = SQLDriver.cmd.ExecuteReaderAsync().Result;
 
                 result.Read();
-                request.credit -= ((Convert.ToDouble(result[0])));
+                int val = Convert.ToInt32(result[0]);
                 result.Close();
+
+
+                if (request.credit < 0)
+                {
+                    SQLDriver.cmd.CommandText = $"SELECT creditcardFees FROM bankIt.accounts WHERE username = \"{request.username}\" && password = \"{request.password}\";";
+                    result = SQLDriver.cmd.ExecuteReaderAsync().Result;
+
+                    result.Read();
+                    request.credit -= ((Convert.ToDouble(result[0])));
+                    result.Close();
+                }
+
+                Double newBalance = val + request.credit;
+
+                SQLDriver.cmd.CommandText = $"UPDATE bankIt.accounts SET creditCard = {newBalance} WHERE username = \"{request.username}\" && password = \"{request.password}\";";
+                if (SQLDriver.cmd.ExecuteNonQueryAsync().Result == 1)
+                {
+                    return $"Credit card balance successfully updated for user '{request.username}'. New balance: {newBalance}.";
+                }
+
+                return $"Failed to update the credit card balance for '{request.username}'. Ensure the account details are correct.";
             }
-
-            Double newBalance = val + request.credit;
-
-            SQLDriver.cmd.CommandText = $"UPDATE bankIt.accounts SET creditCard = {newBalance} WHERE username = \"{request.username}\" && password = \"{request.password}\";";
-            if (SQLDriver.cmd.ExecuteNonQueryAsync().Result == 1)
+            catch (Exception ex)
             {
-                return "Operation Successful!";
+                return StatusCode(500, "An internal error occurred while processing the request. Please try again later.");
             }
-
-            return "Operation Failure!";
         }
     }
 }
