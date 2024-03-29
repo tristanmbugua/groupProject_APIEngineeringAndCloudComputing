8b5e3cc59fa14cbcbe23697b40a5a0f05f45920f
nelsonle345 <97746929+nelsonle345@users.noreply.github.com>
2024-03-29 07:07:18 -0400

    Update SavingsController.cs
    Edited the Feedback

--- bankIt/Controllers/SavingsController.cs
+++ bankIt/Controllers/SavingsController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace bankIt.Controllers
@@ -25,44 +25,59 @@ namespace bankIt.Controllers
         public Object Get([FromBody] ViewSavings request)
         {
             SQLDriver.cmd.CommandText = $"SELECT savings FROM bankIt.accounts WHERE username = \"{request.username}\" && password = \"{request.password}\";";
-            var result = SQLDriver.cmd.ExecuteReaderAsync().Result;
+            using (var result = SQLDriver.cmd.ExecuteReaderAsync().Result)
+            {
+                if (result.Read())
+                {
+                    var savings = Convert.ToInt32(result["savings"]);
+                    result.Close();
+                    return ($"Current savings balance: {savings}");
 
-            result.Read();
-            int resultValue = Convert.ToInt32(result[0]);
-            result.Close();
-
-            return resultValue;
+                }
+                else
+                {
+                    result.Close();
+                    return $"Account not found for username: {request.username}.";
+                }
+            }
         }
 
         [HttpPut]
         public Object Put([FromBody] UpdateSavings request)
         {
-            SQLDriver.cmd.CommandText = $"SELECT savings FROM bankIt.accounts WHERE username = \"{request.username}\" && password = \"{request.password}\";";
-            var result = SQLDriver.cmd.ExecuteReaderAsync().Result;
-
-            result.Read();
-            int val = Convert.ToInt32(result[0]);
-            result.Close();
-
-            if (request.credit < 0)
-            {
-                SQLDriver.cmd.CommandText = $"SELECT savingFees FROM bankIt.accounts WHERE username = \"{request.username}\" && password = \"{request.password}\";";
-                result = SQLDriver.cmd.ExecuteReaderAsync().Result;
+            try {
+                SQLDriver.cmd.CommandText = $"SELECT savings FROM bankIt.accounts WHERE username = \"{request.username}\" && password = \"{request.password}\";";
+               
+                var result = SQLDriver.cmd.ExecuteReaderAsync().Result;
 
                 result.Read();
-                request.credit -= (Convert.ToDouble(result[0]));
+                int val = Convert.ToInt32(result[0]);
                 result.Close();
+
+                if (request.credit < 0)
+                {
+                    SQLDriver.cmd.CommandText = $"SELECT savingFees FROM bankIt.accounts WHERE username = \"{request.username}\" && password = \"{request.password}\";";
+                    result = SQLDriver.cmd.ExecuteReaderAsync().Result;
+
+                    result.Read();
+                    request.credit -= (Convert.ToDouble(result[0]));
+                    result.Close();
+                }
+
+                Double newBalance = val + request.credit;
+
+                SQLDriver.cmd.CommandText = $"UPDATE bankIt.accounts SET savings = {newBalance} WHERE username = \"{request.username}\" && password = \"{request.password}\";";
+                if (SQLDriver.cmd.ExecuteNonQueryAsync().Result == 1)
+                {
+                    return $"Savings updated successfully. New balance: {newBalance}.";
+                }
+
+                return "Failed to update savings. Please check the account details and try again.";
             }
-
-            Double newBalance = val + request.credit;
-
-            SQLDriver.cmd.CommandText = $"UPDATE bankIt.accounts SET savings = {newBalance} WHERE username = \"{request.username}\" && password = \"{request.password}\";";
-            if (SQLDriver.cmd.ExecuteNonQueryAsync().Result == 1)
+            catch (Exception ex)
             {
-                return "Operation Successful!";
+                return StatusCode(500, "An error occurred while updating savings.");
             }
-
-            return "Operation Failure!";
         }
     }
 }
